a2340284d4e27708b8451d43768006f4ae5c32cc
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2020-11-01 09:57:01 +0100

    COMPLETE REFACTORING for new syntax of macro 'func

--- compile.lisp
+++ compile.lisp
@@ -15,65 +15,45 @@
 
 (in-package :clgo)
 
-(func find-obj (scope name)
-  (declare (type (or null goscope) scope)
-           (type symbol name))
-  (loop while scope
-        do (let* ((objs (goscope-objs scope))
-                  (obj  (htable@ objs name)))
-             (if obj
-                 (return-from find-obj (the goobj obj))
-                 (setf scope (goscope-parent scope))))))
+(func find-obj ((scope (or null goscope)) (name symbol)) ((or null goobj))
+  (let ((sc scope))
+    (loop while sc do
+      (let* ((objs (goscope-objs sc))
+             (obj  (htable@ objs name)))
+        (if obj
+            (return-from find-obj (the goobj obj))
+            (setf sc (goscope-parent sc)))))))
 
-(func find-type (scope name)
-  (declare (type (or null goscope) scope)
-           (type symbol name))
-  (loop while scope
-        do (let* ((types (goscope-types scope))
-                  (typ   (htable@ types name)))
-             (if typ
-                 (return-from find-type (the type.go typ))
-                 (setf scope (goscope-parent scope))))))
+(func find-type ((scope (or null goscope)) (name symbol)) ((or null type.go))
+  (let ((sc scope))
+    (loop while sc do
+      (let* ((types (goscope-types sc))
+             (typ   (htable@ types name)))
+        (if typ
+            (return-from find-type (the type.go typ))
+            (setf sc (goscope-parent sc)))))))
 
-(func decl-obj (scope obj)
-  (declare (type goscope scope)
-           (type goobj   obj))
+(func decl-obj ((scope goscope) (obj goobj)) ()
   (htable! (goscope-objs scope) (goobj-name obj) obj)
   (values))
 
-(func decl-type (scope name typ)
-  (declare (type goscope scope)
-           (type symbol  name)
-           (type type.go typ))
+(func decl-type ((scope goscope) (name symbol) (typ type.go)) ()
   (htable! (goscope-types scope) name typ)
   (values))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(func c-find-obj (c name)
-  (declare (type gocompiler c)
-           (type symbol name))
-  (the (values (or null goobj) &optional)
-       (find-obj (gocompiler-scope c) name)))
+(func c-find-obj ((c gocompiler) (name symbol)) ((or null goobj))
+  (find-obj (gocompiler-scope c) name))
 
-(func c-find-type (c name)
-  (declare (type gocompiler c)
-           (type symbol name))
-  (the (values (or null type.go) &optional)
-       (find-type (gocompiler-scope c) name)))
+(func c-find-type ((c gocompiler) (name symbol)) ((or null type.go))
+  (find-type (gocompiler-scope c) name))
 
-(func c-decl-obj (c obj)
-  (declare (type gocompiler c)
-           (type goobj obj))
-  (the (values &optional)
-       (decl-obj (gocompiler-scope c) obj)))
+(func c-decl-obj ((c gocompiler) (obj goobj)) ()
+  (decl-obj (gocompiler-scope c) obj))
 
-(func c-decl-type (c name typ)
-  (declare (type gocompiler c)
-           (type symbol name)
-           (type type.go typ))
-  (the (values &optional)
-       (decl-type (gocompiler-scope c) name typ)))
+(func c-decl-type ((c gocompiler) (name symbol) (typ type.go)) ()
+  (decl-type (gocompiler-scope c) name typ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
